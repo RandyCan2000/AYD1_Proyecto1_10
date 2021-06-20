3dea1c86e865f353544a30f2508feaf69b8171b6
Sergio10RPR <chejoperez.com10@gmail.com>
2021-06-20 02:52:50 -0600

    Agregando Modulos

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors';
 const app = express();
 
 import indexRoutes from './routes/index'
 
 //middlewares
 app.set('port',process.env.PORT || 3000);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
